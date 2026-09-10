343b2f4ebc8e89a112f87dd5c934dd981150def1
Nikplay0 <kollemanronni003@gmail.com>
2026-09-10 09:52:43 +0300

    Лекция 1 по ОАИП(пайтон)

---
+++ test-2.py
@@ -0,0 +1,129 @@
+
+# '''
+# from enum import nonmember
+# from stringprep import c7_set
+
+# print ("hello world")
+# print (2+4)
+
+#     #print (2+4)
+#     #print (2+4)
+
+# if 1<2:
+#     print("привет")
+# else:
+#     print("пока")
+
+# #стандар рер8
+
+# #регистрозависимый
+# #Print("привкт") неверно
+
+# #комметарии
+# #строчные
+# print("ривет") #вывод информации на консоль
+# #описание
+# #вывод на консоль слова привет
+
+# #таблица ключевых слов, которыми нельзя называть переменную
+#  False await else import pass
+# None break except in raise
+# True class finally is return
+# and continue for lambda try
+# as def from nonlocal while
+# assert del global not with
+# async elif if or yield'''
+
+# name = "анастасия"
+# print(type(name))
+
+# #два типа наменования переменных
+
+# username="Fil"
+
+# #underscore notation
+# age=23
+# Age=23
+
+# age=34
+
+# print(age)
+
+# #Числовые типы данных
+
+# int- Целые числа x=5
+# float - вещественный тип данных(число с плавающей точкой) y=5.6
+
+
+
+# #Текстовые типы данных
+# str - Строка, состоящие из символов, заключенных в одинарные или двойные кавычки
+# name="Фил"
+
+# #Логические типы данных
+
+# bool - Логические значения (True-истина,False-ложь)
+
+# is_activate=True
+
+
+# #Cписки и коллекции
+
+# #None
+
+# NoneType- особый тип данных, который представляет отсутствие значения 
+# z=None
+
+
+# x=5
+# y=5.6
+# is_activate=False
+# r=None
+# name="Fil"
+# name1='Fil'
+
+# print(x,y,is_activate,r,name,name1)
+
+# #print(значение, end-конечный символ)
+# #print("привет мир", end=' ')
+# #print("привет мир", end='и пока')
+
+# name= input("Введите имя ")
+# print(name)
+
+# age= int(input("Введите возраст "))
+
+# print(age)
+# #2-10
+# a=0b11
+# b=0b1011
+# print(a,b)
+# #8-10
+# a1=0o7
+# b1=0o11
+# print(a1,b1)
+# #16-10
+# s=0x0A
+# print(s)
+
+# n=input('Введите имя')
+# print("привет",n, "!")
+
+# a=int(input("Введите число: "))
+# b=int(input("Введите число: "))
+# print(a,b)
+
+# f=float(input("введите длинну"))
+# f1=float(input("введите ширину"))
+# z=f*f1
+# print(z)
+
+# #Третья задача
+# # a = int(input("Введите первое число: "))
+# # b = int(input("Введите первое число: "))
+
+# # perimetr = (a + b)*2 
+# # ploshad = a * b
+
+# # print("Периметр прямоугольник:", perimetr, "Площадь прямоугольника", ploshad)
+# '''
